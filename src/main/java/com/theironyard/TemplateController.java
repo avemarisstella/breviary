87666e2534cb2b127fc2e06c952ba20175c9a189
root <root@stellamaris.mi>
2018-04-22 13:31:28 +0200

    Make view part of periodical texts9

--- src/main/java/com/theironyard/TemplateController.java
+++ src/main/java/com/theironyard/TemplateController.java
@@ -36,6 +36,25 @@ public class TemplateController {
         return "home";
     }
 
+
+    @RequestMapping(path = "/thyme")
+    public String home2(Model model){
+
+        if(periodicaltextsRepository.count() == 0) {
+            //ArrayList<Periodicaltexts> periodicaltexts = new ArrayList<>();
+
+            /*for(int x = 0 ; x < 10 ; x++) {
+                widgets.add(new Widget("Example Widget " + x, "Widget " + x + " is very big", new Random().nextDouble() * 100));
+            }*/
+            // periodicaltexts.add(new Periodicaltexts(41,"Hymn","Nieszpory","Okres Adwentu do dnia 16 grudnia","",1,"1 Stwórco gwiaździstych przestworzy\\nI wieczne światło wierzących,\\nChryste, coś wszystkich odkupił,\\nWysłuchaj naszą modlitwę.\\n2 Zdjęty litością nad światem,\\nKtóremu śmierć zagrażała,\\nJego zbawieniem się stałeś\\nI uzdrowiłeś z niemocy.\\n3 Świat już pogrążał się w mroku,\\nLecz Ty, jak słońce promienne,\\nZ łona Dziewicy wyszedłeś,\\nZrodzony przez Nią w czystości.\\n4 Byty niebiańskie i ziemskie,\\nPosłuszne Twoim rozkazom,\\nHołd Ci składają w pokorze\\nI sławią Twoją potęgę.\\n5 Ciebie, Najświętszy, prosimy:\\nNim przyjdziesz sądzić sumienia,\\nObroń nas w życiu doczesnym\\nOd przewrotności szatana.\\n6 Chryste, nasz Królu łagodny,\\Niech Tobie z Ojcem i Duchem\\nBędzie podzięka i chwała\\nPrzez całą wieczność bez kresu. Amen."));
+            // periodicaltextsRepository.save(periodicaltexts);
+        }
+
+        model.addAttribute("periodicaltexts", periodicaltextsRepository.findAll());
+
+        return "index";
+    }
+
    /*@RequestMapping(path = "/")
     public String home(Model model){
        ArrayList<Periodicaltexts> breviary_elements = new ArrayList<>();
