d93f030ca2c598e910d8aab5d5769c5c642c99cb
root <root@stellamaris.mi>
2018-04-25 19:53:07 +0200

    Add thymeleaf 4

--- src/main/java/com/theironyard/controller/Home.java
+++ src/main/java/com/theironyard/controller/Home.java
@@ -1,13 +1,16 @@
 package com.theironyard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
 
     @RequestMapping("/home")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("periodicaltexts", periodicaltextsRepository.findAll());
         return "index";
     }
 
